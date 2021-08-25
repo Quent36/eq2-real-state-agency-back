7ff0849618f74668bc7b5ea6eb7c2b32da405a59
vasc0nce <vasconcelos.corentin@gmail.com>
2021-08-25 13:45:16 +0200

    ajout des classes logement, maison, adresse, appartement et du fichier application.properties

---
+++ src/main/java/com/inti/entities/Adresse.java
@@ -0,0 +1,33 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Adresse implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_adresse")
+	private Long id;
+	private String ville;
+	private String rue;
+	private int numero;
+	private int codePostal;
+	private String complement;
+	
+}
---
+++ src/main/java/com/inti/entities/Maison.java
@@ -0,0 +1,26 @@
+package com.inti.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
+@DiscriminatorValue(value = "maison")
+public class Maison extends Logement implements Serializable{
+	
+	private boolean jardin;
+	private boolean piscine;
+	private boolean cave;
+
+}
