5f970c41f8fafd19f04438ac1267379f6d1f8e0c
Quent36 <quentinlaperse1@gmail.com>
2021-08-30 17:09:57 +0200

    modif du utilisateur controller+ relation utilisateur /role

--- src/main/java/com/inti/entities/Role.java
+++ src/main/java/com/inti/entities/Role.java
@@ -1,11 +1,15 @@
 package com.inti.entities;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,5 +27,9 @@ public class Role implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id_role;
 	private String libelle;
+	
+	@OneToMany
+	@JsonBackReference
+	private List<Utilisateur> utilisateur;
 
 }
